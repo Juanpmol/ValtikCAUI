4c2d1101fc380e6d31fbe73f32d4b3a85715a07f
juanpmol <jpmol27@gmail.com>
2022-07-31 17:42:14 -0500

    Se crean los componentes nav-catalogo-buscar-registro, modulos y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav/nav.component';
+import { CatalogoComponent } from './components/catalogo/catalogo/catalogo.component';
+import { BuscarComponent } from './components/buscar/buscar/buscar.component';
+import { RegistroComponent } from './components/registro/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CatalogoComponent,
+    BuscarComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
